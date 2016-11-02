abac0f2ddd9868e25ec172ac8f7b7167ec729f5c
Brian <guerr119@unlv.nevada.edu>
2016-11-02 13:44:07 -0700

    NREL Solar and PVWatts functional
    Backend is now retrieving the annual sunhours from NREL Solar and
    calculating the correct array system size.

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
  
-sunhours = 6.7 //currently a constant but will be retrieved using NREL data retrievel tool
+// initial sunhours
+var sunhours = 0.0
+//initial PV Array system size
+var systemsize = 0.0;
 
 //Use defined variables
 var address = "6652 Sleepy Meadow Ct."
+var annualload = 6786;
 var direction = 180
 var mount = 0  // 0 = Rack mount , 1 = Roof mount
 if (mount == 0)
@@ -11,26 +15,35 @@ var slope = 36 //latitude
 else
     var slope = 20 //user defined by roof slope
 
-//Function to retrieve annual sun hours/avg
-// function getSunhrs(){
-//      $.getJSON("https://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=7pKM8EGimWnJ8PqI5BA8w2MhEoHyDNqCUSDBPmWn&address=" + address, function(data) { 
-//         console.log(data);// intialize list
-//         console.log(data.outputs.avg_lat_tilt.annual);
-//       });
-// }
-// getSunhurs();
+// NREL solar url
+var nrelURL = "https://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=7pKM8EGimWnJ8PqI5BA8w2MhEoHyDNqCUSDBPmWn&address=" + address;
 
-var annualload = 6786
-//Young PV Array Calculations
-console.log("sunhours = " + sunhours)
-var systemsize = (((annualload/365)/sunhours)/.78)
+function requestJSON(url, callback) {
+	$.ajax({
+		url:url,
+		complete: function(xhr) {
+			callback.call(null, xhr.responseJSON);
+		}
+	});
+}
 
+requestJSON(nrelURL, function(json) {
+  console.log(json);
+  var nrel_sun_hours = json.outputs.avg_lat_tilt.annual;
 
-
+  //Young PV Array Calculations
+  sunhours = nrel_sun_hours;
+  console.log('sunhours from NREL ' + sunhours);
+  systemsize = (((annualload/365)/sunhours)/.78);
+  console.log('PV Array system size is ' + systemsize);
+  
   /* get PV Array info via JSON */
   $.getJSON("https://developer.nrel.gov/api/pvwatts/v5.json?api_key=7pKM8EGimWnJ8PqI5BA8w2MhEoHyDNqCUSDBPmWn&address="+address+"&dataset=tmy3&system_capacity="+systemsize+"&azimuth="+direction+"&tilt="+slope+"&array_type="+mount+"&module_type=1&inv_eff=98&losses=10", function(data) { 
     console.log(data);// intialize list
   });
+})
+
+
 });
 
 
