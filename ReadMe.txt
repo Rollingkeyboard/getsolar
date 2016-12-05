e5f185738287d809aa8e00ff377a713d06b0d1a8
adam.betemedhin@gmail.com <goomer13>
2016-12-05 07:21:46 -0800

    Corrected issues, and add functionality
    Js issues related to api queries and graphing were either corrected or removed.
    
    Prining capabilities were added including a css page to style the page specifically for printing scenarios.
    
    Sysem cost and system payback calculated values were added.
    
    Links for information on incentives related to the sytems being sized were added.
    
    Feedback functionality using an embeded (using ifram and modal) funciton was added to provide useful feed back that can be used to increase the functionality of the website.
    
    Cover image was updated to include a residentail solar installation image.
    
    Webapp description was added at the begining of the webapp to guide users throught the webapp process.
    
    popover functionality was added to increase user assistance as they go through the process of sizing up a system.

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,7 +1,20 @@
-getsolar
+Getsolar
 
 Educational website aimed at assisting individuals with sizing an appropriate solar application to reduce their annual energy cost.
 
 Repo created: 10/18/2016
 
 API: National Renewable Energy Labatory Developer Network
+
+
+
+Instructions
+_____________
+
+- Confirm an active public internet connection
+	*This will be key in utilizing the public api's provided by NREL as well as the google forms document
+
+- Using either google chrome or firefox, open the main.html file located inside of the "getsolar/dev" directory.
+- Once the webapp loads into the web-browser, follow the step provided on the website to opperate the webaapp.
+
+
--- dev/js/main.js
+++ dev/js/main.js
@@ -3,138 +3,148 @@ $(document).ready(solarReady);
 var chart;
 
 function solarReady() {
+    // handle infromative popover fucntions
+    $('[data-toggle="popover"]').popover();
 
-  $('#pv').submit(function(e) {
-    e.preventDefault();
-    
-    var sunhours = 0.0,         // initial sunhours
-        systemsize = 0.0;       //initial PV Array system size
-
-    var formData = getFormData($(this));
-
-    var elecRate = formData.elecRatePerKWH || false;
-
-    // check if address is valid
-    var str_address = formData.address;
-    var split_address = str_address.split(",");
-    var address_validation = validateAddress(split_address);
-    if (address_validation == false) {
-      formData.address = false;
-      $('.jumbotron').children().eq(2).addClass("has-danger");
-    } else {
-      $('.jumbotron').children().eq(2).removeClass("has-danger");
-    }
-    console.log(formData.address);
-
-    // if(formData.address == '6412 Burgundy way'){
-    //   formData.address = false;
-    // }
-
-    if (formData.address && formData.annualElecUsage && formData.mounting && formData.mountDirection) {
-
-      $('#submit').val('Loading...').attr('disabled', true);
+    $('body').on('click', function(e) {
+        //only buttons
+        if ($(e.target).data('toggle') !== 'popover' &&
+            $(e.target).parents('.popover.in').length === 0) {
+            $('[data-toggle="popover"]').popover('hide');
+        }
+    });
+    $('body').popover({
+        selector: '[rel=popover]',
+        trigger: "click"
+    }).on("show.bs.popover", function(e) {
+        // hide all other popovers
+        $("[rel=popover]").not(e.target).popover("destroy");
+        $(".popover").remove();
+    });
+    // handle infromative popover fucntions
 
 
-      var address = formData.address || '6652 Sleepy Meadow Ct.';
-      var annualload = formData.annualElecUsage || 6786;
-      var direction = formData.mountDirection || 180;
-      
-      var mount = formData.mounting;         // 0 = Ground mount , 1 = Roof mount
-      
-      if (mount == 0)
-        var slope = 36 //latitude
-      else
-        var slope = 20 //user defined by roof slope
+    $('#pv').submit(function(e) {
+        e.preventDefault();
 
-      // NREL solar url
-      var solarResource = "https://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=7pKM8EGimWnJ8PqI5BA8w2MhEoHyDNqCUSDBPmWn&address=" + address;
-      var utilityRates = "https://developer.nrel.gov//api/utility_rates/v3.json?api_key=7pKM8EGimWnJ8PqI5BA8w2MhEoHyDNqCUSDBPmWn&address=" + address;
+        var sunhours = 0.0, // initial sunhours
+            systemsize = 0.0; //initial PV Array system size
 
-      $.getJSON(solarResource, function(solarData) {
-        var nrel_sun_hours = solarData.outputs.avg_lat_tilt.annual;
+        var formData = getFormData($(this));
 
-        // Yung PV Array Calculations
-        sunhours = nrel_sun_hours;
-        // console.log('Sunhours from NREL ' + sunhours);
-        
-        systemsize = (((annualload / 365) / sunhours) / .78);
-        // console.log('PV Array system size is ' + systemsize);
+        var elecRate = formData.elecRatePerKWH || false;
 
-        var pvWatts = "https://developer.nrel.gov/api/pvwatts/v5.json?api_key=7pKM8EGimWnJ8PqI5BA8w2MhEoHyDNqCUSDBPmWn&address=" + address + "&dataset=tmy3&system_capacity=" + systemsize + "&azimuth=" + direction + "&tilt=" + slope + "&array_type=" + mount + "&module_type=1&inv_eff=98&losses=10"
+        // check if address is valid
+        var str_address = formData.address;
+        var split_address = str_address.split(",");
+        var address_validation = validateAddress(split_address);
+        if (address_validation == false) {
+            formData.address = false;
+            $('.jumbotron').children().eq(2).addClass("has-danger");
+        } else {
+            $('.jumbotron').children().eq(2).removeClass("has-danger");
+        }
+        console.log(formData.address);
 
-        // get PV Array info via JSON
-        $.getJSON(pvWatts, function(pvData) {
+        // if(formData.address == '6412 Burgundy way'){
+        //   formData.address = false;
+        // }
 
-          $.getJSON(utilityRates, function(utilityData) {
-            if (!formData.elecRatePerKWH) {
-              elecRate = Math.floor(utilityData.outputs.residential * 100) / 100
-              $('#elecRatePerKWH').attr('placeholder', elecRate);
-            }
+        if (formData.address && formData.annualElecUsage && formData.mounting && formData.mountDirection) {
 
-             // ac_monthly data
-            var ac_monthly_pre = pvData.outputs.ac_monthly;
-            // ac_monthly array for new formatted data
-            var ac_monthly = [];
-            // format ac_monthly
-            ac_monthly_pre.forEach(function(num) {
-              ac_monthly.push(Math.round(num*100)/100);
+            $('#submit').val('Loading...').attr('disabled', true);
+
+
+            var address = formData.address || '4505 S Maryland Pkwy, Las Vegas, NV 89154';
+            var annualload = formData.annualElecUsage || 6786;
+            var direction = formData.mountDirection || 180;
+
+            var mount = formData.mounting; // 0 = Ground mount , 1 = Roof mount
+
+            if (mount == 0)
+                var slope = 36
+            else
+                var slope = 20 //user defined by roof slope
+
+            // NREL solar url
+            var solarResource = "https://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=7pKM8EGimWnJ8PqI5BA8w2MhEoHyDNqCUSDBPmWn&address=" + address;
+            var utilityRates = "https://developer.nrel.gov//api/utility_rates/v3.json?api_key=7pKM8EGimWnJ8PqI5BA8w2MhEoHyDNqCUSDBPmWn&address=" + address;
+
+            $.getJSON(solarResource, function(solarData) {
+                var nrel_sun_hours = solarData.outputs.avg_lat_tilt.annual;
+
+                // Yung PV Array Calculations
+                sunhours = nrel_sun_hours;
+                // console.log('Sunhours from NREL ' + sunhours);
+
+                systemsize = (((annualload / 365) / sunhours) / .78);
+                systemcost = (systemsize * 3290) //system size * cost per(kw)
+                    // console.log('PV Array system size is ' + systemsize);
+
+                var pvWatts = "https://developer.nrel.gov/api/pvwatts/v5.json?api_key=7pKM8EGimWnJ8PqI5BA8w2MhEoHyDNqCUSDBPmWn&address=" + address + "&dataset=tmy3&system_capacity=" + systemsize + "&azimuth=" + direction + "&tilt=" + slope + "&array_type=" + mount + "&module_type=1&inv_eff=98&losses=10"
+
+                // get PV Array info via JSON
+                $.getJSON(pvWatts, function(pvData) {
+                    //get utility rate information via JSON
+                    $.getJSON(utilityRates, function(utilityData) {
+                        if (!formData.elecRatePerKWH) {
+                            elecRate = Math.floor(utilityData.outputs.residential * 100) / 100
+                            $('#elecRatePerKWH').attr('placeholder', elecRate);
+                        }
+                        //output pv and finicial informaiton form system the the web-browser
+                        $('#output .capacity').text(Math.ceil(pvData.inputs.system_capacity * 10) / 10);
+                        $('#output .production').text(Math.floor(pvData.outputs.ac_annual));
+                        $('#output .earnings').text(Math.floor(pvData.outputs.ac_annual * elecRate));
+
+                        $('#output .withSystem').text(
+                            Math.ceil(
+                                ((formData.annualElecUsage * elecRate) - (pvData.outputs.ac_annual * elecRate)) * 1
+                            ) / 1
+                        );
+                        $('#output .withoutSystem').text(
+                            Math.ceil(
+                                (formData.annualElecUsage * elecRate) * 1
+                            ) / 1
+                        );
+
+                        $('#output .systemcost').text(
+                            Math.ceil(systemcost)
+                        );
+                        $('.systemcost, .withSystem, .withoutSystem, .earnings').formatCurrency();
+
+                        $('#output .payback').text(
+                            Math.ceil(systemcost / (pvData.outputs.ac_annual * elecRate))
+                        );
+
+                        $('#output').css({
+                            'opacity': 1
+                        });
+                        $('#submit').val('Recalculate Size').attr('disabled', false);
+                    });
+
+                });
             });
-            // if data is present in chart remove it
-            if (chart != undefined) {
-              options.series.pop();
-              console.log('data in chart');
-            }
-            // add plot data
-            options.series.push( {
-              name: 'AC Monthly', 
-              data: ac_monthly 
-            } );
-            // create chart
-            chart = new Highcharts.Chart(options);
 
-            $('#output .capacity').text(Math.ceil(pvData.inputs.system_capacity * 10)/10);
-            $('#output .production').text(Math.floor(pvData.outputs.ac_annual));
-            $('#output .earnings').text(Math.floor(pvData.outputs.ac_annual * elecRate));
-            
-            $('#output .withSystem').text(
-              Math.ceil(
-                ((formData.annualElecUsage * elecRate) - (pvData.outputs.ac_annual * elecRate)) * 1
-              ) / 1
-            );
-            $('#output .withoutSystem').text(
-              Math.ceil(
-                (formData.annualElecUsage * elecRate) * 1
-              ) / 1
-            );
-
-            $('#output').css({ 'opacity' : 1 });
-            $('#submit').val('Recalculate Size').attr('disabled', false);
-          }); 
-
-        });
-      });
-
-    }
-
-  });
+        }
+    });
 }
 
-function getFormData($form){
-  var unindexed_array = $form.serializeArray();
-  var indexed_array = {};
+function getFormData($form) {
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
 
-  $.map(unindexed_array, function(n, i){
-      indexed_array[n['name']] = n['value'];
-  });
+    $.map(unindexed_array, function(n, i) {
+        indexed_array[n['name']] = n['value'];
+    });
 
-  return indexed_array;
+    return indexed_array;
 }
 
 function requestJSON(url, callback) {
-  $.ajax({
-    url: url,
-    complete: function(xhr) {
-      callback.call(null, xhr.responseJSON);
-    }
-  });
+    $.ajax({
+        url: url,
+        complete: function(xhr) {
+            callback.call(null, xhr.responseJSON);
+        }
+    });
 }
